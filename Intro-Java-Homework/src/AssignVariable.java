c65eeeaf9399a325b74f80f7107cce273a94e4b7
KaloianDimitrov <malkatamravka@abv.bg>
2016-03-24 03:50:46 +0200

    Create AssignVariable.java

---
+++ Intro-Java-Homework/src/AssignVariable.java
@@ -0,0 +1,13 @@
+public class AssignVariable {
+    public static void main(String[] args) {
+        boolean a = false;
+        short b = 32767;
+        int c = 200000000;
+        double d = 0.1234567891011;
+        float e = 0.5f;
+        long f = 919827112351L;
+        byte g = 127;
+        char h = 'c';
+
+    }
+}
